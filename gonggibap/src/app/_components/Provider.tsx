eaf154bcfb716b3e2cba1052062ec7dc856a8949
limjunhee <limjunhee4575@gmail.com>
2024-10-21 10:35:11 +0900

    chore : toast 설치, 셋팅

--- gonggibap/src/app/_components/Provider.tsx
+++ gonggibap/src/app/_components/Provider.tsx
@@ -3,12 +3,14 @@
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer, ToastContainerProps } from "react-toastify";
 
 export const Provider = ({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) => {
+  // tanstack-query
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -18,9 +20,24 @@ export const Provider = ({
     },
   });
 
+  // toastMessage
+  const toastConfig: ToastContainerProps = {
+    position: "bottom-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    newestOnTop: false,
+    closeOnClick: true,
+    rtl: false,
+    pauseOnFocusLoss: true,
+    draggable: true,
+    pauseOnHover: true,
+    theme: "light",
+  };
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ToastContainer {...toastConfig} />
       {process.env.NODE_ENV === "development" && (
         <ReactQueryDevtools initialIsOpen={false} />
       )}
