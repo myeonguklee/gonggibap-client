14787e2a20d1279a7179cf266f2c59f4832dd63d
limjunhee <limjunhee4575@gmail.com>
2024-10-21 13:16:02 +0900

    feat : 카카오맵 추가

--- gonggibap/src/app/page.tsx
+++ gonggibap/src/app/page.tsx
@@ -1,3 +1,47 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import Script from "next/script";
+
+declare global {
+  interface Window {
+    kakao: {
+      maps: {
+        load: (callback: () => void) => void;
+        LatLng: new (lat: number, lng: number) => any;
+        Map: new (container: HTMLElement, options: any) => any;
+      };
+    };
+  }
+}
+
 export default function Home() {
-  return <div>메인페이지</div>;
+  const mapRef = useRef<HTMLDivElement>(null);
+  const [mapLoaded, setMapLoaded] = useState(false);
+
+  useEffect(() => {
+    if (mapLoaded && mapRef.current) {
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      new window.kakao.maps.Map(mapRef.current, options);
+    }
+  }, [mapLoaded]);
+
+  const onKakaoMapLoad = () => {
+    window.kakao.maps.load(() => setMapLoaded(true));
+  };
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+        onLoad={onKakaoMapLoad}
+      />
+      <div ref={mapRef} style={{ width: "100%", height: "500px" }} />
+    </>
+  );
 }
