7dd9b896fbdbcce668eb134413250fb15b93415c
myeonguklee <muk525@nate.com>
2024-10-24 10:46:47 +0900

    feat: 구글 애널리틱스 추가

---
+++ gonggibap/src/app/_components/GoogleAnalytics.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import ReactGA from 'react-ga4';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+type EventProps = {
+  category: string;
+  action: string;
+  label?: string;
+  value?: number;
+};
+
+export const logEvent = ({ category, action, label, value }: EventProps) => {
+  ReactGA.event({
+    category,
+    action,
+    label,
+    value,
+  });
+};
+
+export const logPageView = (page: string) => {
+  ReactGA.send({ hitType: "pageview", page });
+};
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+export function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    ReactGA.initialize(GA_MEASUREMENT_ID!);
+  }, []);
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+    ReactGA.send({ hitType: "pageview", page: url });
+  }, [pathname, searchParams]);
+
+  return null;
+}
+
+// export default function SearchButton() {
+//   const handleSearch = () => {
+//     // 검색 로직
+    
+//     // 이벤트 추적
+//     logEvent({
+//       category: 'Search',
+//       action: 'Click',
+//       label: 'Restaurant Search',
+//       value: 1
+//     });
+//   };
+
+//   return <button onClick={handleSearch}>검색</button>;
+// }
