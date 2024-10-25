b229b4e04741513259f1e104a6f1f2fb30e6cc40
myeonguklee <muk525@nate.com>
2024-10-25 17:53:02 +0900

    feat: 리뷰 생성 리액트 쿼리

--- gonggibap/src/apis/review/useCreateReview.ts
+++ gonggibap/src/apis/review/useCreateReview.ts
@@ -1,7 +1,53 @@
-const createReview = async(): Promise<void> => {
-  // Review 생성 로직
+import { AxiosError } from "axios";
+import { UseMutationResult, useMutation } from "@tanstack/react-query";
+import { BaseResponse, ErrorResponse } from "@/types/apiResponse";
+import { client } from "@/apis/core/client";
+
+interface CreateReviewDTO {
+  restaurantId: number;
+  content: string;
+  point: number;
+  images?: File[];
 }
 
-export const useCreateReview = (): () => void => {
-  return createReview;
+const createReview = async ({
+  restaurantId,
+  content,
+  point,
+  images,
+}: CreateReviewDTO): Promise<void> => {
+  const formData = new FormData();
+
+  // required
+  formData.append("restaurantId", restaurantId.toString());
+  formData.append("content", content);
+  formData.append("point", point.toString());
+
+  // optional
+  if (images && images.length > 0) {
+    images.forEach((image) => {
+      formData.append("images", image);
+    });
+  }
+
+  const response = await client.post<BaseResponse<void>>({
+    url: "reviews",
+    data: formData,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return response.data;
+};
+
+export const useCreateReview = (): UseMutationResult<
+  void,
+  AxiosError<ErrorResponse>,
+  CreateReviewDTO,
+  unknown
+> => {
+  return useMutation({
+    mutationFn: createReview,
+  });
 };
