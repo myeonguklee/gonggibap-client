439cf3ce2f92317c60ae9f72be08c9b894eb96d7
limjunhee <limjunhee4575@gmail.com>
2024-10-25 17:58:32 +0900

    feat : api 호출 회수 감소

---
+++ gonggibap/src/hooks/useCurrentLocation.ts
@@ -0,0 +1,27 @@
+import { useState, useCallback } from "react";
+import { Coordinate } from "@/types/restaurant";
+
+interface UseCurrentLocationReturn {
+  getCurrentLocation: () => Promise<Coordinate>; // 위치를 가져오는 함수
+}
+
+export const useCurrentLocation = (): UseCurrentLocationReturn => {
+  const getCurrentLocation = useCallback(async (): Promise<Coordinate> => {
+    try {
+      const position = await new Promise<GeolocationPosition>(
+        (resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        }
+      );
+      return {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+    } catch (err) {
+      const error =
+        err instanceof Error ? err : new Error("Failed to get location");
+      throw error;
+    }
+  }, []);
+  return { getCurrentLocation };
+};
