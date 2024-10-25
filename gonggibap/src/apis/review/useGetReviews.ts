75b443d241adb62984b3d52d9928c377c9563671
myeonguklee <muk525@nate.com>
2024-10-25 17:41:34 +0900

    style: 코드 컨벤션에 맞춰 수정

--- gonggibap/src/apis/review/useGetReviews.ts
+++ gonggibap/src/apis/review/useGetReviews.ts
@@ -1,9 +1,9 @@
-import { Review } from "@/types/review";
-import { client } from "../core/client";
-import { BaseResponse, ErrorResponse } from "@/types/apiResponse";
-import { UseQueryResult, useQuery } from "@tanstack/react-query";
-import { QUERY_KEYS } from "@/constants/queryKeys";
 import { AxiosError } from "axios";
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { BaseResponse, ErrorResponse } from "@/types/apiResponse";
+import { Review } from "@/types/review";
+import { client } from "@/apis/core/client";
+import { QUERY_KEYS } from "@/constants/queryKeys";
 
 const getReviews = async (restaurantId: number): Promise<Review[]> => {
   const response = await client.get<BaseResponse<Review[]>>({
