0ea45eb61869bad704afb73e52556a1072a0955a
myeonguklee <muk525@nate.com>
2024-10-29 12:12:55 +0900

    feat: 페이지네이션 구현

--- gonggibap/src/app/_components/RestaurantListView.tsx
+++ gonggibap/src/app/_components/RestaurantListView.tsx
@@ -1,19 +1,29 @@
+import { Fragment, useState } from "react";
 import { Footprints, Star } from "lucide-react";
 import { Restaurant } from "@/types/restaurant";
 import { event } from "@/app/_components/GoogleAnalytics";
+import { Pagination } from "@/app/_components/Pagination";
 
 type RestaurantListViewProps = {
   restaurants: Restaurant[];
   onRestaurantSelect: (restaurant: Restaurant) => void;
   selectedRestaurantId?: number;
-  totalPpage: number;
+  totalPages: number;
 };
 
 export const RestaurantListView: React.FC<RestaurantListViewProps> = ({
   restaurants,
   onRestaurantSelect,
   selectedRestaurantId,
+  totalPages,
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChage = (page: number) => {
+    setCurrentPage(page);
+    // 페이지 변경에 따른 API 호출 등 로직
+  };
+
   const handleRestaurantSelect = (restaurant: Restaurant) => {
     // GA4 이벤트 발생, 기본적인 선택 이벤트
     event({
@@ -36,53 +46,60 @@ export const RestaurantListView: React.FC<RestaurantListViewProps> = ({
   };
 
   return (
-    <ul className="flex flex-col gap-2">
-      {restaurants.length === 0 && (
-        <p className="text-center">검색된 식당이 없습니다.</p>
-      )}
-      {restaurants.map((restaurant, index) => (
-        <li key={restaurant.restaurantId}>
-          <button
-            onClick={() => handleRestaurantSelect(restaurant)}
-            className={`w-full text-left flex flex-col gap-2 p-4 rounded-lg transition-colors dark:bg-gray-700 border dark:border-none
+    <Fragment>
+      <ul className="flex flex-col gap-2">
+        {restaurants.length === 0 && (
+          <p className="text-center">검색된 식당이 없습니다.</p>
+        )}
+        {restaurants.map((restaurant, index) => (
+          <li key={restaurant.restaurantId}>
+            <button
+              onClick={() => handleRestaurantSelect(restaurant)}
+              className={`w-full text-left flex flex-col gap-2 p-4 rounded-lg transition-colors dark:bg-gray-700 border dark:border-none
             ${
               selectedRestaurantId === restaurant.restaurantId
                 ? "bg-gray-100 dark:bg-gray-900"
                 : "hover:bg-gray-100 dark:hover:bg-gray-900"
             }`}
-          >
-            <div className="w-full flex-between-center">
-              <div className="flex flex-col gap-2">
-                <h3 className="text-base font-bold">
-                  {index + 1}. {restaurant.restaurantName}
-                </h3>
-                <p className="text-sm">
-                  <span className="sr-only">음식점 카테고리</span>
-                  {restaurant.restaurantCategory}
-                </p>
-              </div>
-              <div className="flex flex-col gap-1">
-                <div className="flex items-center gap-1 text-yellow-400">
-                  <span className="sr-only">음식점 평점</span>
-                  <Star size="1rem" />
-
-                  {restaurant.pointAvg ? (restaurant.pointAvg.toFixed(1)):("-")}
+            >
+              <div className="w-full flex-between-center">
+                <div className="flex flex-col gap-2">
+                  <h3 className="text-base font-bold">
+                    {index + 1}. {restaurant.restaurantName}
+                  </h3>
+                  <p className="text-sm">
+                    <span className="sr-only">음식점 카테고리</span>
+                    {restaurant.restaurantCategory}
+                  </p>
                 </div>
-                <div className="flex items-center gap-1 text-[#FF9A00]">
-                  <span className="sr-only">방문 횟수: </span>
-                  <Footprints size="1rem" />
-                  <p className="text-base">{restaurant.visitCount}</p>
+                <div className="flex flex-col gap-1">
+                  <div className="flex items-center gap-1 text-yellow-400">
+                    <span className="sr-only">음식점 평점</span>
+                    <Star size="1rem" />
+
+                    {restaurant.pointAvg ? restaurant.pointAvg.toFixed(1) : "-"}
+                  </div>
+                  <div className="flex items-center gap-1 text-[#FF9A00]">
+                    <span className="sr-only">방문 횟수: </span>
+                    <Footprints size="1rem" />
+                    <p className="text-base">{restaurant.visitCount}</p>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <address className="text-gray-400 text-xs text-single-line not-italic">
-              {restaurant.restaurantRoadAddressName}
-            </address>
-          </button>
-        </li>
-      ))}
-    </ul>
+              <address className="text-gray-400 text-xs text-single-line not-italic">
+                {restaurant.restaurantRoadAddressName}
+              </address>
+            </button>
+          </li>
+        ))}
+      </ul>
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChage}
+      />
+    </Fragment>
   );
 };
 
