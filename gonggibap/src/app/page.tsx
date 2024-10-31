275292e14862778dddc539e1bb97a7ade42c68e9
myeonguklee <muk525@nate.com>
2024-10-31 13:39:48 +0900

    feat: 현 지도에서 재검색시 페이지 0 설정

--- gonggibap/src/app/page.tsx
+++ gonggibap/src/app/page.tsx
@@ -105,6 +105,7 @@ export default function Home() {
           clearMapMarkers();
           handleSearch();
           setSelectedRestaurantId(null);
+          handlePageChange(0);
         }}
         className="fixed left-1/2 -translate-x-1/2 md:left-[calc(50%+10rem)] top-20 font-semibold md:top-auto md:bottom-12 flex-center gap-1 bg-[#FF7058] text-white px-4 py-2 md:px-6 md:py-3 text-base md:text-lg rounded-2xl shadow-lg hover:bg-[#FF6147] z-10 focus:outline-none"
         aria-label="현 지도에서 재검색"
