9d50ed565abb2a9b6c70f5d561b833f683e564eb
myeonguklee <muk525@nate.com>
2024-10-31 17:54:26 +0900

    feat: 사용내역 페이지네이션, 로딩 상태 구현

--- gonggibap/src/app/_components/sidebar/history/HistoryContent.tsx
+++ gonggibap/src/app/_components/sidebar/history/HistoryContent.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { Pagination } from "@/app/_components/Pagination";
+import { MapPinLoading } from "@/app/_components/MapPinLoading";
 import { useGetHistories } from "@/apis/history";
 
 interface HistoryContentProps {
@@ -5,18 +8,48 @@ interface HistoryContentProps {
 }
 
 export function HistoryContent({ restaurantId }: HistoryContentProps) {
-  const { data: histories } = useGetHistories(restaurantId);
+  const [currentPage, setCurrentPage] = useState(0);
+  const { data: histories, isLoading } = useGetHistories(
+    restaurantId,
+    currentPage
+  );
+
+  const handleHistoryPageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [restaurantId]);
+
+  if (isLoading) {
+    return <MapPinLoading />;
+  }
+
   return (
-    <div className="flex flex-col gap-5">
-      {histories?.content.map((history, index) => (
-        <div key={index}>
-          <p>사용자: {history.publicOfficeName} {history.consumer}</p>
-          <p>가격: {history.price}</p>
-          <p>인원수: {history.peopleCount}</p>
-          <p>사용 목적: {history.useContent}</p>
-          <p>날짜: {history.historyDate}</p>
+    <>
+      {histories?.content.length === 0 ? (
+        <div className="flex flex-col gap-5">
+          {histories?.content.map((history, index) => (
+            <div key={index}>
+              <p>
+                사용자: {history.publicOfficeName} {history.consumer}
+              </p>
+              <p>가격: {history.price}</p>
+              <p>인원수: {history.peopleCount}</p>
+              <p>사용 목적: {history.useContent}</p>
+              <p>날짜: {history.historyDate}</p>
+            </div>
+          ))}
+          <Pagination
+            totalPages={histories?.totalPages ? histories.totalPages : 1}
+            currentPage={currentPage}
+            onPageChange={handleHistoryPageChange}
+          />
         </div>
-      ))}
-    </div>
+      ) : (
+        <p className="text-center">검색된 이용 내역이 없습니다.</p>
+      )}
+    </>
   );
 }
