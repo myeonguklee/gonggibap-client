c9255fb20497cd9db43bc835141bb2f466eabd4f
limjunhee <limjunhee4575@gmail.com>
2024-11-01 10:24:58 +0900

    design/#68/카테고리 디자인

--- gonggibap/src/app/_components/CategoryFilter.tsx
+++ gonggibap/src/app/_components/CategoryFilter.tsx
@@ -5,6 +5,7 @@ import { RestaurantDetailCategory } from "@/types/restaurant";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { getCategoryIcon } from "@/utils/getCategoryIcon";
 import { RESTAURANT_CATEGORIES } from "@/constants/category";
+
 interface CategoryFilterProps {
   selectedCategory: RestaurantDetailCategory;
   onSelectCategory: (category: RestaurantDetailCategory) => void;
@@ -17,33 +18,35 @@ export const CategoryFilter = ({
   const [isOpen, setIsOpen] = useState(false);
   const isMobile = useMediaQuery("(max-width: 768px)");
 
-  // 메인에 보여줄 카테고리
   const MAIN_CATEGORIES = isMobile
     ? RESTAURANT_CATEGORIES.slice(0, 2)
-    : RESTAURANT_CATEGORIES.slice(0, 3);
+    : RESTAURANT_CATEGORIES.slice(0, 2);
 
   const OTHER_CATEGORIES = isMobile
     ? RESTAURANT_CATEGORIES.slice(2)
-    : RESTAURANT_CATEGORIES.slice(3);
+    : RESTAURANT_CATEGORIES.slice(2);
 
   const CategoryButton = ({
     value,
     label,
+    isMoreMenu = false,
   }: {
     value: RestaurantDetailCategory | null;
     label: string;
+    isMoreMenu?: boolean;
   }) => (
     <button
       onClick={() => {
         onSelectCategory(value);
         if (isMobile) setIsOpen(false);
       }}
-      className={`px-3 py-1.5 rounded-2xl text-sm whitespace-nowrap transition-colors shadow-md flex items-center gap-2 w-full justify-center
-            ${
-              selectedCategory === value
-                ? "bg-[#FF7058] font-semibold text-white hover:bg-[#FF8068]"
-                : "bg-white font-semibold text-gray-500 hover:bg-gray-200 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600"
-            }`}
+      className={`px-3 py-1.5 text-sm whitespace-nowrap transition-colors flex items-center gap-2 w-full
+          ${!isMoreMenu ? "shadow-md rounded-2xl" : ""}
+          ${
+            selectedCategory === value
+              ? "bg-[#FF7058] font-semibold text-white hover:bg-[#FF8068]"
+              : "bg-white font-semibold text-gray-500 hover:bg-gray-200 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600"
+          }`}
     >
       {getCategoryIcon(value, "w-4 h-4")}
       {label}
@@ -52,10 +55,19 @@ export const CategoryFilter = ({
 
   // 더보기에 표시될 카테고리 목록
   const MoreCategories = () => (
-    <div className="grid grid-cols-2 gap-2">
-      {OTHER_CATEGORIES.map((category) => (
-        <div key={category.key} className="w-full">
-          <CategoryButton value={category.value} label={category.value} />
+    <div className="grid grid-cols-2">
+      {OTHER_CATEGORIES.map((category, index) => (
+        <div
+          key={category.key}
+          className={`border-b border-r border-gray-200 dark:border-gray-600 
+            ${index >= OTHER_CATEGORIES.length - 2 ? "border-b-0" : ""} 
+            [&:nth-child(2n)]:border-r-0`}
+        >
+          <CategoryButton
+            value={category.value}
+            label={category.value}
+            isMoreMenu={true}
+          />
         </div>
       ))}
     </div>
@@ -93,7 +105,7 @@ export const CategoryFilter = ({
                 <h2 className="ml-2 text-lg font-semibold mb-4 text-gray-800 dark:text-gray-200">
                   카테고리
                 </h2>
-                <div className="px-2">
+                <div>
                   <MoreCategories />
                 </div>
               </Dialog.Content>
@@ -109,7 +121,7 @@ export const CategoryFilter = ({
 
             <Popover.Portal>
               <Popover.Content
-                className="bg-white dark:bg-black rounded-lg p-2 w-[280px] z-50"
+                className="bg-white dark:bg-black rounded-lg overflow-hidden w-[280px] z-50"
                 sideOffset={5}
                 align="end"
               >
