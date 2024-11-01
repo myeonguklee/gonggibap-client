c4a4ce459be99c3ee2adebd5ba1679ae9f9e8bb8
limjunhee <limjunhee4575@gmail.com>
2024-11-01 17:54:31 +0900

    design : 상세페이지 디자인

--- gonggibap/src/app/_components/sidebar/review/index.ts
+++ gonggibap/src/app/_components/sidebar/review/index.ts
@@ -2,3 +2,4 @@ export { ReviewForm } from "@/app/_components/sidebar/review/ReviewForm";
 export { ReviewsContent } from "@/app/_components/sidebar/review/ReviewsContent";
 export { ReviewImages } from "@/app/_components/sidebar/review/ReviewImages";
 export { ReviewList } from "@/app/_components/sidebar/review/ReviewList";
+export { StarRating } from "@/app/_components/sidebar/review/StarRating";
--- gonggibap/src/types/review.ts
+++ gonggibap/src/types/review.ts
@@ -2,6 +2,8 @@ export interface Review {
   reviewId: number;
   userId: number;
   userName: string;
+  userReviewAvg: number;
+  userReviewCount: number;
   point: number;
   content: string;
   date: string;
