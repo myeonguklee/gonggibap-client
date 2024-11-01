76d5ccd1b9b59d6b72439c5eb7e6d7cf5746a5ea
myeonguklee <muk525@nate.com>
2024-11-01 13:21:12 +0900

    feat: 찜한 식당 조회 커스텀 훅(리액트 쿼리)

---
+++ gonggibap/src/apis/favorite/useGetFavoriteRestaurants.ts
@@ -0,0 +1,22 @@
+import { AxiosError } from "axios";
+import { useSuspenseQuery, UseSuspenseQueryResult } from "@tanstack/react-query";
+import { BaseResponse, ErrorResponse } from "@/types/apiResponse";
+import { client } from "@/apis/core/client";
+import { QUERY_KEYS } from "@/constants/queryKeys";
+import { GetRestaurantsResponse } from "@/types/restaurant";
+
+const getFavoriteRestaurants = async (): Promise<GetRestaurantsResponse> => {
+  const response = await client.get<BaseResponse<GetRestaurantsResponse>>({
+    url: "restaurants/favorite/",
+  });
+  return response.data;
+};
+
+export const usGetFavoriteRestaurants = (
+): UseSuspenseQueryResult<GetRestaurantsResponse, AxiosError<ErrorResponse>> => {
+  return useSuspenseQuery<GetRestaurantsResponse, AxiosError<ErrorResponse>>({
+    queryKey: [QUERY_KEYS.FAVORITE.ALL],
+    queryFn: () => getFavoriteRestaurants(),
+    staleTime: 1000 * 60 * 5,
+  });
+};
