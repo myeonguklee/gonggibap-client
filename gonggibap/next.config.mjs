eb328b9c64f0efee308b8f3051bee36e226a7650
thswltjr11 <thswltjr11@gmail.com>
2024-11-07 20:09:42 +0900

    feat: #154 hotjar 설정

--- gonggibap/next.config.mjs
+++ gonggibap/next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ['gonggibap.s3.ap-northeast-2.amazonaws.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
