1d35374032c05b3d67826de122bed20af02b7a6c
myeonguklee <muk525@nate.com>
2024-11-08 16:06:39 +0900

    feat: 사용 내역 조회 prefetch 추가

--- gonggibap/src/apis/history/useGetHistories.ts
+++ gonggibap/src/apis/history/useGetHistories.ts
@@ -1,4 +1,4 @@
-import { UseQueryResult, useQuery } from '@tanstack/react-query';
+import { UseQueryResult, useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { BaseResponse, ErrorResponse } from '@/types/apiResponse';
@@ -7,6 +7,8 @@ import { GetHistoriesResponse } from '@/types/history';
 import { client } from '@/apis/core/client';
 
 import { QUERY_KEYS } from '@/constants/queryKeys';
+import { useEffect } from 'react';
+import { getVisiblePageNumbers } from '@/utils/getVisiblePageNumbers ';
 
 const getHistories = async (
   restaurantId: number,
@@ -26,10 +28,32 @@ export const useGetHistories = (
   restaurantId: number,
   page: number,
 ): UseQueryResult<GetHistoriesResponse, AxiosError<ErrorResponse>> => {
-  return useQuery<GetHistoriesResponse, AxiosError<ErrorResponse>>({
-    queryKey: [QUERY_KEYS.HISTORY.DETAIL(restaurantId, page)],
-    queryFn: () => getHistories(restaurantId, page),
-    enabled: !!restaurantId,
-    staleTime: 1000 * 60 * 5,
-  });
-};
+  const queryClient = useQueryClient();  
+
+    // 기본 쿼리
+    const query = useQuery<GetHistoriesResponse, AxiosError<ErrorResponse>>({
+      queryKey: QUERY_KEYS.HISTORY.DETAIL(restaurantId, page),
+      queryFn: () => getHistories(restaurantId, page),
+      enabled: !!restaurantId,
+      staleTime: Infinity,
+    });
+  
+    // 프리페칭 로직
+    useEffect(() => {
+      if (restaurantId && query.data?.totalPages) {
+        const visiblePages = getVisiblePageNumbers(page, query.data.totalPages);
+        
+        visiblePages.forEach((targetPage) => {
+          if (targetPage !== page) {
+            queryClient.prefetchQuery({
+              queryKey: QUERY_KEYS.HISTORY.DETAIL(restaurantId, targetPage),
+              queryFn: () => getHistories(restaurantId, targetPage),
+              staleTime: 1000 * 60 * 5,
+            });
+          }
+        });
+      }
+    }, [queryClient, restaurantId, page, query.data?.totalPages]);
+  
+    return query;
+  };
