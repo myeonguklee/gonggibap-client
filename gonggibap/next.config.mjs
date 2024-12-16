2504ba1a6a619e9d069076b69fbe8fc7715b1c1d
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-12-16 11:10:43 +0900

    fix: 이미지 설정 롤백

--- gonggibap/next.config.mjs
+++ gonggibap/next.config.mjs
@@ -2,9 +2,17 @@
 const nextConfig = {
   images: {
     domains: ['gonggibap.s3.ap-northeast-2.amazonaws.com'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp'],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'gonggibap.s3.ap-northeast-2.amazonaws.com',
+    //     port: '',
+    //     pathname: '/**',
+    //   },
+    // ],
+    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    // formats: ['image/webp'],
   },
   async headers() {
     return [
